63b655d8a6666426a3c7fd4b4870bda56103d4b5
MIN0225 <cro77@naver.com>
2025-04-26 22:32:53 +0900

    로그인 js

--- 3.JS/9.Project/MusicApp/public/js/login.js
+++ 3.JS/9.Project/MusicApp/public/js/login.js
@@ -1,4 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+  const form = document.querySelector('form');
   
+  form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const username = form.username.value;
+    const password = form.password.value;
+
+    const response = await fetch('/api/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        username,
+        password,
+      })
+    })
+    // const data = await response.json();
+
+    if (response.ok) {
+      console.log("로그인 성공");
+      window.location.href = '/home.html';
+    } else {
+      console.log("로그인 실패");
+    }
+  })
 })
